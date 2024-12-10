0093db15880a6dcbb5ce5e1ceede1f169e561d1f
Ivy Wong <ivy@ivywong.dev>
2024-12-10 10:34:31 -0800

    add guess input field

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState('');
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    console.log({ guess });
+    setGuess('');
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        pattern="[a-zA-Z]{5}"
+        minLength={5}
+        maxLength={5}
+        title="Must be a 5 letter word"
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
